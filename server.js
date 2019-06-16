55f9db5a9667d308128a9bf57077852168ba79e3
Ahmed OSAMA <nt7647@nsgbeg.local>
2019-06-16 09:08:18 +0200

    prepare for heroku

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const app = express();
 
+//hosting the dist folder
 app.use(express.static(__dirname + '/dist'));
 
+//api endpoints goes here...
+
+
+//catching all invalid routes for sloving the same problem we had when deploying 
+//our app to firebase this happens when we try to refresh the followers page
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
+//use defualt port 80 or if it not supplied we use 8080
 app.listen(process.env.PORT || 8080);
